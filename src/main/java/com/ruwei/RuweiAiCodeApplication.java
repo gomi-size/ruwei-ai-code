cd5b16f06845eef0b32413743dce3525632ec050
wudi <3136124236@qq.com>
2026-05-23 10:29:58 +0800

    项目初始化

---
+++ src/main/java/com/ruwei/RuweiAiCodeApplication.java
@@ -0,0 +1,13 @@
+package com.ruwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuweiAiCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuweiAiCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruwei/RuweiAiCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruwei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuweiAiCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
