ed033fc47e95615929460b1a2aa537d561f5a150
wudi <3136124236@qq.com>
2026-05-29 23:11:55 +0800

    历史对话——完全实现对话记忆存储

--- src/main/java/com/ruwei/ai/AICodeGeneratorService.java
+++ src/main/java/com/ruwei/ai/AICodeGeneratorService.java
@@ -2,6 +2,7 @@ package com.ruwei.ai;
 
 import com.ruwei.ai.model.HtmlCodeResult;
 import com.ruwei.ai.model.MultiFileCodeResult;
+import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import reactor.core.publisher.Flux;
 
@@ -41,6 +42,14 @@ public interface AICodeGeneratorService {
      */
     @SystemMessage(fromResource = "prompt/bb.txt")
     Flux<String> generateMultiFileCodeStream(String userMessage);
+    /**
+     * 生成Vue项目代码生成流式
+     * @param userMessage 用户提示词
+     * @MemoryId Long appId
+     * @return AI输出结果
+     */
+    @SystemMessage(fromResource = "prompt/cc.txt")
+    Flux<String> generateVueProjectCodeStream(@MemoryId Long appId, String userMessage);
 
 
 }
--- src/main/java/com/ruwei/ai/AICodeGeneratorServiceFactory.java
+++ src/main/java/com/ruwei/ai/AICodeGeneratorServiceFactory.java
@@ -2,18 +2,19 @@ package com.ruwei.ai;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.ruwei.ai.tools.FileWriteTool;
+import com.ruwei.exception.BusinessException;
+import com.ruwei.exception.ErrorCode;
+import com.ruwei.model.enums.CodeGenTypeEnum;
 import com.ruwei.service.ChatHistoryService;
 import dev.langchain4j.community.store.memory.chat.redis.RedisChatMemoryStore;
+import dev.langchain4j.data.message.ToolExecutionResultMessage;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.StreamingChatModel;
 import dev.langchain4j.service.AiServices;
-import dev.langchain4j.service.memory.ChatMemoryService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Duration;
@@ -29,7 +30,10 @@ public class AICodeGeneratorServiceFactory {
     private ChatModel chatModel;
 
     @Resource
-    private StreamingChatModel streamingChatModel;
+    private StreamingChatModel openAiStreamingChatModel;
+
+    @Resource
+    private StreamingChatModel reasoningStreamingChatModel;
 
     @Resource
     private RedisChatMemoryStore redisChatMemoryStore;
@@ -40,30 +44,41 @@ public class AICodeGeneratorServiceFactory {
     /**
      * AI服务实例缓存
      */
-    private final Cache<Long, AICodeGeneratorService> cache = Caffeine.newBuilder()
+    private final Cache<String, AICodeGeneratorService> cache = Caffeine.newBuilder()
             .maximumSize(200)
             .expireAfterWrite(Duration.ofMillis(30))
             .expireAfterAccess(Duration.ofMillis(10))
             .removalListener((key, value, cause) -> {
-                log.debug("AI服务实例被移除，appId:{},原因：{}",key, cause);
+                log.debug("AI服务实例被移除，缓存键:{},原因：{}",key, cause);
             }).build();
 
     /**
-     * 根据appid获取服务
+     * 根据appid获取服务(兼容老方法)
      * @param appId
      * @return
      */
     public AICodeGeneratorService getAICodeGeneratorService(Long appId) {
         //根据appId构建独立的对话记忆
-        return cache.get(appId, this::createAICodeGeneratorService);
+        return getAICodeGeneratorService(appId, CodeGenTypeEnum.MULTI_FILE);
     }
-
     /**
-     * 创建新的ai服务，初始化AI新服务
+     * 根据appid获取服务
      * @param appId
      * @return
      */
-    public AICodeGeneratorService createAICodeGeneratorService(Long appId) {
+    public AICodeGeneratorService getAICodeGeneratorService(Long appId,CodeGenTypeEnum codeGenTypeEnum) {
+        String buildCacheKey = buildCacheKey(appId, codeGenTypeEnum);
+        //根据appId构建独立的对话记忆
+        return cache.get(buildCacheKey, key->createAICodeGeneratorService(appId,codeGenTypeEnum));
+    }
+
+    /**
+     * 创建新的AI服务，(这个是创建更为复杂的ai服务)
+     * @param appId
+     * @param codeGenTypeEnum
+     * @return
+     */
+    public AICodeGeneratorService createAICodeGeneratorService(Long appId, CodeGenTypeEnum codeGenTypeEnum) {
         log.info("为{}，创建新的AI服务",appId);
         //根据appId构建独立的对话记忆
         MessageWindowChatMemory chatMemory = MessageWindowChatMemory
@@ -74,11 +89,34 @@ public class AICodeGeneratorServiceFactory {
                 .build();
         //从数据库中加载对话记忆
         chatHistoryService.loadChatHistoryMemory(appId, chatMemory,20);
-        return AiServices.builder(AICodeGeneratorService.class)
-                .chatModel(chatModel)
-                .streamingChatModel(streamingChatModel)
-                // 根据 id 构建独立的对话记忆
-                .chatMemory(chatMemory)
-                .build();
+        return switch (codeGenTypeEnum){
+            //Vue项目生成就使用工具调用和项目推理
+            case VUE_PROJECT -> AiServices.builder(AICodeGeneratorService.class)
+                    .chatModel(chatModel)
+                    .streamingChatModel(reasoningStreamingChatModel)
+                    // 根据 id 构建独立的对话记忆
+                    .chatMemoryProvider(memory->chatMemory)
+                    .tools(new FileWriteTool())
+                    //处理工具幻觉问题
+                    .hallucinatedToolNameStrategy(toolExecutionRequest ->
+                            ToolExecutionResultMessage.from(toolExecutionRequest,
+                                    "Error :there is no tool called"+toolExecutionRequest.name()))
+                    .build();
+            //普通的就是HTML和多文件生成，使用流式对话模型
+            case HTML ,MULTI_FILE->AiServices.builder(AICodeGeneratorService.class)
+                    .chatModel(chatModel)
+                    .streamingChatModel(openAiStreamingChatModel)
+                    // 根据 id 构建独立的对话记忆
+                    .chatMemory(chatMemory)
+                    .build();
+            default -> throw new BusinessException(ErrorCode.SYSTEM_ERROR,"不支持的代码生成类型");
+        };
+
+    }
+    /**
+     * 构建缓存键
+     */
+    private String buildCacheKey(long appId, CodeGenTypeEnum codeGenType) {
+        return appId + "_" + codeGenType.getValue();
     }
 }
